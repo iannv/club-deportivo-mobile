bb219f777897a1cf1522a626bcb716ae3462bcc6
Ian Vázquez <cam.iva@hotmail.com>
2025-04-30 23:35:08 -0300

    Login y menú principal

---
+++ app/src/main/java/com/example/club_deportivo_mobile/comprobante_pago.kt
@@ -0,0 +1,20 @@
+package com.example.club_deportivo_mobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class comprobante_pago : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_comprobante_pago)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
