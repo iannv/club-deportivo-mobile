1f1f7e62d009f1d86cc6545300c62b56ce4f2870
Ian Vázquez <cam.iva@hotmail.com>
2025-05-01 21:50:10 -0300

    Registro de nuevo cliente

--- app/src/main/java/com/example/club_deportivo_mobile/registrar_cliente.kt
+++ app/src/main/java/com/example/club_deportivo_mobile/registrar_cliente.kt
@@ -1,6 +1,8 @@
 package com.example.club_deportivo_mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class registrar_cliente : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var volver = findViewById<ImageView>(R.id.img_volver)
+
+        volver.setOnClickListener {
+            var intent = Intent(this, menuPrincipal::class.java)
+            startActivity(intent)
         }
     }
 }
