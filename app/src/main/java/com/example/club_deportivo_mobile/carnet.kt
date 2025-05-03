d1237b8ad280cbe81a04530a130bc2dfa6c37c57
Ian Vázquez <cam.iva@hotmail.com>
2025-05-03 19:14:31 -0300

    Activity carnet

--- app/src/main/java/com/example/club_deportivo_mobile/carnet.kt
+++ app/src/main/java/com/example/club_deportivo_mobile/carnet.kt
@@ -1,20 +1,30 @@
 package com.example.club_deportivo_mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class carnet : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_carnet)
+
+        var volver = findViewById<ImageView>(R.id.img_volver2)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        volver.setOnClickListener {
+            startActivity(Intent(this, menuPrincipal::class.java))
         }
     }
 }
