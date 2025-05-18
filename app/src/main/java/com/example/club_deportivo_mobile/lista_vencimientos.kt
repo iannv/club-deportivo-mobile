b2623415b7efd766a866eef6b56f388e34c11f6e
Seba2107 <146895879+Seba2107@users.noreply.github.com>
2025-05-17 22:47:27 -0300

    modificacion del boton retorno

--- app/src/main/java/com/example/club_deportivo_mobile/lista_vencimientos.kt
+++ app/src/main/java/com/example/club_deportivo_mobile/lista_vencimientos.kt
@@ -1,12 +1,18 @@
 package com.example.club_deportivo_mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class lista_vencimientos : AppCompatActivity() {
+
+    private lateinit var imgRetorno: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,13 @@ class lista_vencimientos : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        imgRetorno = findViewById(R.id.img_volver_buscar)
+
+        imgRetorno.setOnClickListener {
+            startActivity(Intent(this, menuPrincipal::class.java))
+            finish()
         }
     }
 }
